58e82955fa5f472f1ea6b5aadfcca1eb1a426422
alexander-daniel <alex.vados@gmail.com>
2015-01-02 12:39:29 -0800

    init

---
+++ README.md
@@ -0,0 +1,24 @@
+# nom-de-guerre
+a simple random naming module. more fun than numbered ID's sometimes.
+could be extended to use different lists of names...
+
+defaults to returning a `string` of a random place in Québec.
+
+# Usage
+```javascript
+var randomPlaceName = require('nom-de-guerre');
+
+randomPlaceName() // -> will output random place in Québec
+```
+
+# Methods
+## randomPlaceName()
+returns a random name - `string`.
+
+# Installation
+```bash
+npm install random-place-name
+```
+
+# License
+MIT
--- index.js
+++ index.js
@@ -6,13 +6,15 @@ module.exports = randomName;
 names.used = {};
 
 function randomName() {
-  if ( Object.keys( names.used ).length === names.length - 1) {
-    console.log(Object.keys( names.used ).length);
-    names.used = {};
-  }
+    if ( Object.keys( names.used ).length === names.length - 1) {
+        console.log(Object.keys( names.used ).length);
+        names.used = {};
+    }
 
-  var random = names[ Math.floor( Math.random() * names.length ) ];
-  names.used[ random ] = 1;
-  return random;
+    var index = Math.floor(Math.random()*names.length);
+    var random = names[index];
+    names.used[ random ] = 1;
+    return random;
 }
 
+console.log(randomName());
