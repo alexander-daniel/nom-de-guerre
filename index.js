f4e82e21c4738923d9d9d2ecc788ecbd37032808
alexander-daniel <alex.vados@gmail.com>
2015-11-17 11:20:47 -0500

    new countries, census

--- index.js
+++ index.js
@@ -1,18 +1,70 @@
 'use strict';
 
-var names = require('./names.json').names;
+const DEFAULT_NAMES = require('./data/quebec.json').names;
+const NAMES = require('./data/complete.json');
 
-module.exports = randomName;
-names.used = {};
+/*
+ * Regular random name generator, will just spit out a random place
+ * in quebec, but not check if unique.
+ */
+const nomDeGuerre = module.exports = function (countryName) {
+    if (!countryName) countryName = 'Canada';
 
-function randomName() {
-    if ( Object.keys( names.used ).length === names.length - 1) {
-        console.log(Object.keys( names.used ).length);
-        names.used = {};
+    let citiesList = NAMES[countryName];
+
+    if (!citiesList.length) {
+        throw new Error(`${countryName} does not have a cities list yet...`);
     }
 
-    var index = Math.floor(Math.random()*names.length);
-    var random = names[index];
-    names.used[ random ] = 1;
+    const index = Math.floor( Math.random() * DEFAULT_NAMES.length);
+    const random = DEFAULT_NAMES[index];
+
     return random;
-}
+};
+
+/* Helper function to get the names of all possible countries */
+nomDeGuerre.getCountries = function () {
+    return Object.keys(NAMES);
+};
+
+/* Helper function to get the cities from a country */
+nomDeGuerre.getCountry = function (countryName) {
+    return NAMES[countryName];
+};
+
+/**
+ * @param {country} String of country you want to use the cities list of
+ * @returns {census} Function that will return a unique city from the cities list selected.
+ *                   will output each item only once.
+ */
+nomDeGuerre.census = function (countryName) {
+    if (!countryName) countryName = 'Canada';
+
+    let citiesList = NAMES[countryName];
+
+    if (!citiesList.length) {
+        throw new Error(`${countryName} does not have a cities list yet...`);
+    }
+
+    let censusList = citiesList.slice();
+
+    /*
+     * This is what we return, and it has the list in its closure scope, so we
+     * can lop off the used names as we go
+     */
+    function census () {
+
+        if (!censusList.length) {
+            throw new Error('No more unique names to choose from!');
+        }
+
+        const index = Math.floor( Math.random() * censusList.length);
+        const random = censusList[index];
+
+        censusList.splice(index, 1);
+
+        return random;
+    }
+
+    return census;
+};
